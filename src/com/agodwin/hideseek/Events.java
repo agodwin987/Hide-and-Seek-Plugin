bc5ca003214fd59cf4995cffbd7e190db0f01e39
Alex Godwin <agodwin987@gmail.com>
2013-03-25 12:03:08 -0400

    Added place event

--- src/com/agodwin/hideseek/Events.java
+++ src/com/agodwin/hideseek/Events.java
@@ -3,11 +3,18 @@ package com.agodwin.hideseek;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class Events implements Listener {
 	@EventHandler
 	public void onBreak(BlockBreakEvent e) {
 		if (Main.inArena.containsKey(e.getPlayer().getName())) {
+			e.setCancelled(true);
+		}
+	}
+	@EventHandler
+	public void onPlace(BlockPlaceEvent e){
+		if (Main.inArena.containsKey(e.getPlayer().getName())){
 			e.setCancelled(true);
 		}
 	}
