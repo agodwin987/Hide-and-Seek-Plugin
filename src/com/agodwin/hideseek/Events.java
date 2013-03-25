5a381780217249d978bc343ebeacb813e7869cd3
Alex Godwin <agodwin987@gmail.com>
2013-03-25 12:57:28 -0400

    Added new events for damage

--- src/com/agodwin/hideseek/Events.java
+++ src/com/agodwin/hideseek/Events.java
@@ -1,9 +1,11 @@
 package com.agodwin.hideseek;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class Events implements Listener {
 	@EventHandler
@@ -16,6 +18,13 @@ public class Events implements Listener {
 	public void onPlace(BlockPlaceEvent e){
 		if (Main.inArena.containsKey(e.getPlayer().getName())){
 			e.setCancelled(true);
+		}
+	}
+	@EventHandler
+	public void onHit(EntityDamageByEntityEvent e){
+		if(e.getDamager().hasMetadata("seek")&&e.getDamager() instanceof Player){
+			e.getEntity().setMetadata("seek", null);
+			
 		}
 	}
 }
